c0ff7d2cbd308c99d6087c151bbf109a8c739549
ingcarlosmontenegro <ingcarlosmontenegro@gmail.com>
2021-04-05 04:53:32 +0000

    ejercicio 002

---
+++ 002-Creating-Variables-and-Printing-2/Main.java
@@ -0,0 +1,7 @@
+class Main {
+  public static void main(String[] args) {
+    //write your code here!
+    
+    System.out.println(age);
+  }
+}
---
+++ 002-Creating-Variables-and-Printing-2/README.md
@@ -0,0 +1,5 @@
+# Creating Variables and Printing 2
+
+For you to do:
+
+1. Create an integer variable on line 4 and set it to the value 21.
